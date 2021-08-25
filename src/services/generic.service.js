85db281bdcb0dff6c4fa1532529374358caf140f
Namaskaro <45168111+Namaskaro@users.noreply.github.com>
2021-08-26 00:12:12 +0300

    Update generic.service.js

--- src/services/generic.service.js
+++ src/services/generic.service.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const API_URL = "http://localhost:3000/api/v1";
+const API_URL = "http://176.99.11.6:3005/api/v1";
 
 export const request = async ({ url, method, data }) => {
   const response = await axios[method](`${API_URL}/${url}`, data);
